c53f981daecb6b6678ddee30ed44024f64907fb8
sdrapeau <Stephane.Drapeau@obeo.fr>
2012-05-22 10:55:45 +0200

    Updated copyright headers

--- org.obeonetwork.dsl.bpmn2.design/src/org/obeonetwork/bpmn2/design/Activator.java
+++ org.obeonetwork.dsl.bpmn2.design/src/org/obeonetwork/bpmn2/design/Activator.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2011-2012 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ * 
+ */
 package org.obeonetwork.bpmn2.design;
 
 import fr.obeo.dsl.viewpoint.business.api.componentization.ViewpointRegistry;
