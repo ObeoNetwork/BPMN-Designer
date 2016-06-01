add9ab9e739412115c3b1d00d3c5dfa5753de618
jbrazeau <jean-francois.brazeau@obeo.fr>
2016-06-01 14:58:41 +0200

    Fix JIRA 94 & 95 : processes that can be selected for a CallActivity + node position & size kept when a task type changes

--- plugins/org.obeonetwork.dsl.bpmn2.design/src/org/obeonetwork/bpmn2/design/TaskService.java
+++ plugins/org.obeonetwork.dsl.bpmn2.design/src/org/obeonetwork/bpmn2/design/TaskService.java
@@ -1,6 +1,7 @@
 package org.obeonetwork.bpmn2.design;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sirius.diagram.DNodeContainer;
 import org.obeonetwork.dsl.bpmn2.Bpmn2Factory;
 import org.obeonetwork.dsl.bpmn2.BusinessRuleTask;
 import org.obeonetwork.dsl.bpmn2.CallActivity;
@@ -31,7 +32,7 @@ public class TaskService {
 
 		boolean isLikeScriptTask = eObject instanceof ScriptTask;
 
-		if(!isLikeScriptTask && eObject instanceof CallActivity) {
+		if (!isLikeScriptTask && eObject instanceof CallActivity) {
 			CallActivity callActivity = (CallActivity) eObject;
 			isLikeScriptTask = callActivity.getCalledElementRef() instanceof GlobalScriptTask;
 		}
@@ -39,12 +40,12 @@ public class TaskService {
 		return isLikeScriptTask;
 
 	}
-	
+
 	public boolean isLikeBusinessRuleTask(final EObject eObject) {
 
 		boolean isLikeBusinessRuleTask = eObject instanceof BusinessRuleTask;
 
-		if(!isLikeBusinessRuleTask && eObject instanceof CallActivity) {
+		if (!isLikeBusinessRuleTask && eObject instanceof CallActivity) {
 			CallActivity callActivity = (CallActivity) eObject;
 			isLikeBusinessRuleTask = callActivity.getCalledElementRef() instanceof GlobalBusinessRuleTask;
 		}
@@ -52,12 +53,12 @@ public class TaskService {
 		return isLikeBusinessRuleTask;
 
 	}
-	
+
 	public boolean isLikeUserTask(final EObject eObject) {
 
 		boolean isLikeUserTask = eObject instanceof UserTask;
 
-		if(!isLikeUserTask && eObject instanceof CallActivity) {
+		if (!isLikeUserTask && eObject instanceof CallActivity) {
 			CallActivity callActivity = (CallActivity) eObject;
 			isLikeUserTask = callActivity.getCalledElementRef() instanceof GlobalUserTask;
 		}
@@ -70,7 +71,7 @@ public class TaskService {
 
 		boolean isLikeManualTask = eObject instanceof ManualTask;
 
-		if(!isLikeManualTask && eObject instanceof CallActivity) {
+		if (!isLikeManualTask && eObject instanceof CallActivity) {
 			CallActivity callActivity = (CallActivity) eObject;
 			isLikeManualTask = callActivity.getCalledElementRef() instanceof GlobalManualTask;
 		}
@@ -79,29 +80,33 @@ public class TaskService {
 
 	}
 
-	//<%self.filter("Task")!=null && self.filter("UserTask")==null && self.filter("ServiceTask")==null && self.filter("SendTask")==null && self.filter("ScriptTask")==null && self.filter("ReceiveTask")==null && self.filter("ManualTask")==null && self.filter("BusinessRuleTask")==null && self.filter("CallActivity")==null%>
+	// <%self.filter("Task")!=null && self.filter("UserTask")==null &&
+	// self.filter("ServiceTask")==null && self.filter("SendTask")==null &&
+	// self.filter("ScriptTask")==null && self.filter("ReceiveTask")==null &&
+	// self.filter("ManualTask")==null && self.filter("BusinessRuleTask")==null
+	// && self.filter("CallActivity")==null%>
 	public boolean isLikeTask(final EObject eObject) {
 		return eObject instanceof Task && eObject.getClass().isAssignableFrom(TaskImpl.class);
 	}
-	
+
 	public boolean isLikeReceiveTask(final EObject eObject) {
 		return eObject instanceof ReceiveTask;
 	}
-	
+
 	public boolean isLikeSendTask(final EObject eObject) {
 		return eObject instanceof SendTask;
 	}
-	
+
 	public boolean isLikeServiceTask(final EObject eObject) {
 		return eObject instanceof ServiceTask;
 	}
-	
+
 	public boolean isLikeChoreographyTask(final EObject eObject) {
 		return eObject instanceof ChoreographyTask;
 	}
-	
+
 	private Task clone(Task task, Task cloneTask) {
-		
+
 		cloneTask.getBoundaryEventRefs().addAll(task.getBoundaryEventRefs());
 		cloneTask.getCategoryValueRef().addAll(task.getCategoryValueRef());
 		cloneTask.getDataInputAssociations().addAll(task.getDataInputAssociations());
@@ -116,7 +121,7 @@ public class TaskService {
 		cloneTask.getOutgoingConversationLinks().addAll(task.getOutgoingConversationLinks());
 		cloneTask.getProperties().addAll(task.getProperties());
 		cloneTask.getResources().addAll(task.getResources());
-		
+
 		cloneTask.setAuditing(task.getAuditing());
 		cloneTask.setCompletionQuantity(task.getCompletionQuantity());
 		cloneTask.setDefault(task.getDefault());
@@ -127,118 +132,98 @@ public class TaskService {
 		cloneTask.setMonitoring(task.getMonitoring());
 		cloneTask.setName(task.getName());
 		cloneTask.setStartQuantity(task.getStartQuantity());
-		
+
 		FlowElementsContainer container = (FlowElementsContainer) task.eContainer();
 		container.getFlowElements().remove(task);
 		container.getFlowElements().add(cloneTask);
-		
+
 		return cloneTask;
 	}
-	
-	public Task convertToTask(final EObject eObject) {
-		Task cloneTask = null;
-		
-		if(eObject instanceof Task) {
-			if(eObject.getClass().isAssignableFrom(TaskImpl.class)) {
-				cloneTask = (Task) eObject;
-			} else {
-				cloneTask = Bpmn2Factory.eINSTANCE.createTask();
-				clone((Task) eObject, cloneTask);
-			}
-		}				
-		
-		return cloneTask;
-	}
-	
-	public BusinessRuleTask convertToBusinessRuleTask(final EObject eObject) {
-		BusinessRuleTask cloneTask = null;
-		
-		if(eObject instanceof BusinessRuleTask) {
-			cloneTask = (BusinessRuleTask) eObject;
-		} else if(eObject instanceof Task) {
-			cloneTask = Bpmn2Factory.eINSTANCE.createBusinessRuleTask();
-			clone((Task) eObject, cloneTask);
+
+	public Task convertToTask(final DNodeContainer view) {
+		Task eObject = (Task) view.getTarget();
+		if (!eObject.getClass().isAssignableFrom(TaskImpl.class)) {
+			view.setTarget(clone(eObject, Bpmn2Factory.eINSTANCE.createTask()));
 		}
-		
-		return cloneTask;
+		return eObject;
 	}
-	
-	public ManualTask convertToManualTask(final EObject eObject) {
-		ManualTask cloneTask = null;
-		
-		if(eObject instanceof ManualTask) {
-			cloneTask = (ManualTask) eObject;
-		} else if(eObject instanceof Task) {
-			cloneTask = Bpmn2Factory.eINSTANCE.createManualTask();
-			clone((Task) eObject, cloneTask);
+
+	public BusinessRuleTask convertToBusinessRuleTask(final DNodeContainer view) {
+		Task eObject = (Task) view.getTarget();
+		if (eObject instanceof BusinessRuleTask) {
+			return (BusinessRuleTask) eObject;
+		} else {
+			BusinessRuleTask cloneTask = (BusinessRuleTask) clone(eObject,
+					Bpmn2Factory.eINSTANCE.createBusinessRuleTask());
+			view.setTarget(cloneTask);
+			return cloneTask;
 		}
-		
-		return cloneTask;
 	}
-	
-	public ReceiveTask convertToReceiveTask(final EObject eObject) {
-		ReceiveTask cloneTask = null;
-		
-		if(eObject instanceof ReceiveTask) {
-			cloneTask = (ReceiveTask) eObject;
-		} else if(eObject instanceof Task) {
-			cloneTask = Bpmn2Factory.eINSTANCE.createReceiveTask();
-			clone((Task) eObject, cloneTask);
+
+	public ManualTask convertToManualTask(final DNodeContainer view) {
+		Task eObject = (Task) view.getTarget();
+		if (eObject instanceof ManualTask) {
+			return (ManualTask) eObject;
+		} else {
+			ManualTask cloneTask = (ManualTask) clone(eObject, Bpmn2Factory.eINSTANCE.createManualTask());
+			view.setTarget(cloneTask);
+			return cloneTask;
 		}
-		
-		return cloneTask;
 	}
-	
-	public ScriptTask convertToScriptTask(final EObject eObject) {
-		ScriptTask cloneTask = null;
-		
-		if(eObject instanceof ScriptTask) {
-			cloneTask = (ScriptTask) eObject;
-		} else if(eObject instanceof Task) {
-			cloneTask = Bpmn2Factory.eINSTANCE.createScriptTask();
-			clone((Task) eObject, cloneTask);
+
+	public ReceiveTask convertToReceiveTask(final DNodeContainer view) {
+		Task eObject = (Task) view.getTarget();
+		if (eObject instanceof ReceiveTask) {
+			return (ReceiveTask) eObject;
+		} else {
+			ReceiveTask cloneTask = (ReceiveTask) clone(eObject, Bpmn2Factory.eINSTANCE.createReceiveTask());
+			view.setTarget(cloneTask);
+			return cloneTask;
 		}
-		
-		return cloneTask;
 	}
-	
-	public SendTask convertToSendTask(final EObject eObject) {
-		SendTask cloneTask = null;
-		
-		if(eObject instanceof SendTask) {
-			cloneTask = (SendTask) eObject;
-		} else if(eObject instanceof Task) {
-			cloneTask = Bpmn2Factory.eINSTANCE.createSendTask();
-			clone((Task) eObject, cloneTask);
+
+	public ScriptTask convertToScriptTask(final DNodeContainer view) {
+		Task eObject = (Task) view.getTarget();
+		if (eObject instanceof ScriptTask) {
+			return (ScriptTask) eObject;
+		} else {
+			ScriptTask cloneTask = (ScriptTask) clone(eObject, Bpmn2Factory.eINSTANCE.createScriptTask());
+			view.setTarget(cloneTask);
+			return cloneTask;
 		}
-		
-		return cloneTask;
 	}
-	
-	public ServiceTask convertToServiceTask(final EObject eObject) {
-		ServiceTask cloneTask = null;
-		
-		if(eObject instanceof ServiceTask) {
-			cloneTask = (ServiceTask) eObject;
-		} else if(eObject instanceof Task) {
-			cloneTask = Bpmn2Factory.eINSTANCE.createServiceTask();
-			clone((Task) eObject, cloneTask);
+
+	public SendTask convertToSendTask(final DNodeContainer view) {
+		Task eObject = (Task) view.getTarget();
+		if (eObject instanceof SendTask) {
+			return (SendTask) eObject;
+		} else {
+			SendTask cloneTask = (SendTask) clone(eObject, Bpmn2Factory.eINSTANCE.createSendTask());
+			view.setTarget(cloneTask);
+			return cloneTask;
 		}
-		
-		return cloneTask;
 	}
-	
-	public UserTask convertToUserTask(final EObject eObject) {
-		UserTask cloneTask = null;
-		
-		if(eObject instanceof UserTask) {
-			cloneTask = (UserTask) eObject;
-		} else if(eObject instanceof Task) {
-			cloneTask = Bpmn2Factory.eINSTANCE.createUserTask();
-			clone((Task) eObject, cloneTask);
+
+	public ServiceTask convertToServiceTask(final DNodeContainer view) {
+		Task eObject = (Task) view.getTarget();
+		if (eObject instanceof ServiceTask) {
+			return (ServiceTask) eObject;
+		} else {
+			ServiceTask cloneTask = (ServiceTask) clone(eObject, Bpmn2Factory.eINSTANCE.createServiceTask());
+			view.setTarget(cloneTask);
+			return cloneTask;
 		}
-		
-		return cloneTask;
 	}
-	
+
+	public UserTask convertToUserTask(final DNodeContainer view) {
+		Task eObject = (Task) view.getTarget();
+		if (eObject instanceof UserTask) {
+			return (UserTask) eObject;
+		} else {
+			UserTask cloneTask = (UserTask) clone(eObject, Bpmn2Factory.eINSTANCE.createUserTask());
+			view.setTarget(cloneTask);
+			return cloneTask;
+		}
+	}
+
 }
