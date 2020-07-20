a350e031f671cfd7675f42a08f1c5aac2968ac5b
Stephane Drapeau <Stephane.Drapeau@obeo.fr>
2020-07-20 11:26:05 +0200

    Do not show the identifier in the group label
    Change-Id: I58a089551dc6bf8b053a17aadec30872dab64093

--- plugins/org.obeonetwork.dsl.bpmn2.edit/src-gen/org/obeonetwork/dsl/bpmn2/provider/GroupItemProvider.java
+++ plugins/org.obeonetwork.dsl.bpmn2.edit/src-gen/org/obeonetwork/dsl/bpmn2/provider/GroupItemProvider.java
@@ -19,7 +19,6 @@ import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
@@ -27,22 +26,21 @@ import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
-
 import org.obeonetwork.dsl.bpmn2.Bpmn2Package;
-import org.obeonetwork.dsl.bpmn2.Group;
 
 /**
- * This is the item provider adapter for a {@link org.obeonetwork.dsl.bpmn2.Group} object.
- * <!-- begin-user-doc -->
- * <!-- end-user-doc -->
+ * This is the item provider adapter for a
+ * {@link org.obeonetwork.dsl.bpmn2.Group} object. <!-- begin-user-doc --> <!--
+ * end-user-doc -->
+ * 
  * @generated
  */
 public class GroupItemProvider extends ArtifactItemProvider implements IEditingDomainItemProvider,
 		IStructuredItemContentProvider, ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource {
 	/**
-	 * This constructs an instance from a factory and a notifier.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This constructs an instance from a factory and a notifier. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public GroupItemProvider(AdapterFactory adapterFactory) {
@@ -50,9 +48,9 @@ public class GroupItemProvider extends ArtifactItemProvider implements IEditingD
 	}
 
 	/**
-	 * This returns the property descriptors for the adapted class.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This returns the property descriptors for the adapted class. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -66,9 +64,9 @@ public class GroupItemProvider extends ArtifactItemProvider implements IEditingD
 	}
 
 	/**
-	 * This adds a property descriptor for the Category Value Ref feature.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This adds a property descriptor for the Category Value Ref feature. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected void addCategoryValueRefPropertyDescriptor(Object object) {
@@ -81,9 +79,8 @@ public class GroupItemProvider extends ArtifactItemProvider implements IEditingD
 	}
 
 	/**
-	 * This returns Group.png.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This returns Group.png. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -96,8 +93,8 @@ public class GroupItemProvider extends ArtifactItemProvider implements IEditingD
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -106,23 +103,21 @@ public class GroupItemProvider extends ArtifactItemProvider implements IEditingD
 	}
 
 	/**
-	 * This returns the label text for the adapted class.
-	 * <!-- begin-user-doc -->
+	 * This returns the label text for the adapted class. <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * 
+	 * @generated NOT
 	 */
 	@Override
 	public String getText(Object object) {
-		String label = ((Group) object).getId();
-		return label == null || label.length() == 0 ? getString("_UI_Group_type")
-				: getString("_UI_Group_type") + " " + label;
+		return getString("_UI_Group_type");
 	}
 
 	/**
-	 * This handles model notifications by calling {@link #updateChildren} to update any cached
-	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * This handles model notifications by calling {@link #updateChildren} to update
+	 * any cached children and by creating a viewer notification, which it passes to
+	 * {@link #fireNotifyChanged}. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -132,10 +127,10 @@ public class GroupItemProvider extends ArtifactItemProvider implements IEditingD
 	}
 
 	/**
-	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
-	 * that can be created under this object.
-	 * <!-- begin-user-doc -->
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing
+	 * the children that can be created under this object. <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
